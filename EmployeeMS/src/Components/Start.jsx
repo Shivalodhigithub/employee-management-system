b77f1b5005d66bb605f8d9e3edb6afc615a140fc
Shivalodhigithub <140102719+Shivalodhigithub@users.noreply.github.com>
2024-12-12 23:06:43 +0530

    Update Start.jsx

--- EmployeeMS/src/Components/Start.jsx
+++ EmployeeMS/src/Components/Start.jsx
@@ -1,54 +1,3 @@
-// import React, { useEffect } from 'react'
-// import { Link,useNavigate } from 'react-router-dom'
-// import axios from 'axios'
-// const Start = () => {
-//   const navigate=useNavigate();
-//   axios.defaults.withCredentials = true;
-//   useEffect(()=>{
-//     axios.get('http://localhost:3000/auth/verifyuser').then((result) => {
-//       // console.log(result)
-//       if(result.data.Status){
-//         if(result.data.role === 'admin'){
-          
-//           navigate('/dashboard') 
-//         }
-//         else{
-//           navigate('/employeedetails/'+result.data._id)
-
-//         }
-//       }
- 
-//     }).catch((err) => {
-//       console.log(err)
-      
-//     });
-
-//   },[])
-
-//   return (
-//     <>
-//                 <div className=' d-flex justify-content-center vh-100 align-items-center loginPage'> 
-//             {/* <ToastContainer /> */}
-//                 <div className=' p-3  w-25   rounded border loginForm'> <div className=" text-warning ">
-//                 <h2 className=' text-center'>Login As</h2>
-//                 <div className='d-flex justify-content-around align-items-center'>
-//                     <button className=' btn btn-primary' onClick={()=>{navigate('/employeelogin')}}>Employee</button>
-//                     <button className=' btn btn-primary' onClick={()=>{navigate('/adminlogin')}}>Admin</button>
-//                 </div>
-         
-                       
-//                     </div>
-                     
-                     
-                     
-//                 </div>
-//             </div>
-      
-//     </>
-//   )
-// }
-
-// export default Start
 
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -62,7 +11,7 @@ const Start = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/auth/verifyuser')
+      .get('https://employee-management-system-e3di.onrender.com/auth/verifyuser')
       .then((result) => {
         if (result.data.Status) {
           if (result.data.role === 'admin') {
