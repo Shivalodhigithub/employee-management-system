631a641b808a5b04b686640de73bc57590726cff
Shivalodhigithub <140102719+Shivalodhigithub@users.noreply.github.com>
2024-12-13 00:11:21 +0530

    Update auth.routes.js

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -5,6 +5,10 @@ const authController=require('../controller/auth.controller')
 const authMiddleWare=require('../middle_ware/auth.middle_ware')
 module.exports=(app)=>{
     app.post('/auth/adminlogin' , authController.signIn)
+    app.head('/adminlogin', (req, res) => {
+    res.status(200).send('OK');
+});
+
     app.get('/auth/verifyuser',[authMiddleWare.verifyUser],authController.verify)
     app.get('/auth/adminlogin_cnt' , authController.cntAdmin)
     app.get('/auth/adminlogout',authController.logOutAdmin)
