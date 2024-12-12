ac5eb3866a8c461ca5193fd3409af41e956ed5ee
Shivalodhigithub <140102719+Shivalodhigithub@users.noreply.github.com>
2024-12-12 22:34:15 +0530

    Update url.config.js

--- Backend/config/url.config.js
+++ Backend/config/url.config.js
@@ -1,4 +1,4 @@
 module.exports={
-    dbUrl:'mongodb://localhost/EmployeeMS'
+    dbUrl:'mongodb+srv://Mystore:Mystore1272@cluster0.anqs7fa.mongodb.net/employeedb?retryWrites=true&w=majority'
     // dbUrl:'mongodb://localhost/EmployeeMS'
 }
